1e8fc54f44156304c9d34cf069437f3d5d6a98a4
Eloisa Seabra <seabra.eloisa@gmail.com>
2020-08-10 15:42:50 +0100

    half-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 
 import './App.css';
+
+function MessageComponent() {
+  return (
+    <section>
+      <h1>Say hello to ReactJS</h1>
+      <p>
+        You will learn a FrontEnd framework from scratch, to become an Ninja
+        Developer.
+      </p>
+
+      <button> Awesome </button>
+    </section>
+  );
+}
+
+function Nav() {
+  return (
+    <div>
+      <nav>
+        <img src="images/ironhack-logo.svg" className="App-logo" alt="logo" />
+      </nav>
+    </div>
+  );
+}
+
+function Images() {
+  return (
+    <section>
+      <div className="items">
+        <div className="images">
+          <img src="images/icon1.png" alt="icon1" />
+          <h2>Declarative</h2>
+          <p> React makes it painless to create interactive UIs </p>
+        </div>
+        <div className="images">
+          <img src="images/icon2.png" alt="icon2" />
+          <h2> Components </h2>
+          <p> Build encapsulated components that manage their state </p>
+        </div>
+        <div className="images">
+          <img src="images/icon3.png" alt="icon3" />
+          <h2> Single-Way </h2>
+          <p> A set of immutable values are passed to components </p>
+        </div>
+        <div className="images">
+          <img src="images/icon4.png" alt="icon4" />
+          <h2> JSX </h2>
+          <p> Statically-typed, designed to run on modern browsers </p>
+        </div>
+      </div>
+    </section>
+  );
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Nav />
+          <MessageComponent />
         </header>
+        <body>
+          <Images />
+        </body>
       </div>
     );
   }
